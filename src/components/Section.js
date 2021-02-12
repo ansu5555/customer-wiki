0e17e22ba6852dcb65c043eaa2208f9ca49879e5
ansu5555 <ansuman5555@gmail.com>
2021-02-12 10:36:57 +0530

    implimented two column display

--- src/components/Section.js
+++ src/components/Section.js
@@ -2,21 +2,14 @@ import PropTypes from 'prop-types'
 
 function Section({ text, img, color }) {
   return (
-    <div
-      className='d-flex flex-column section'
-      style={{ backgroundColor: color.bgColor }}>
+    <div className='section' style={{ backgroundColor: color.bgColor }}>
       <div
+        className='section-divider'
         style={{
-          width: '98vw',
           borderTop: `2px solid ${color.fgColor}`,
-          margin: 'auto',
         }}></div>
-      <div className='section-text-wrapper'>
-        <div className='section-text' style={{ color: color.fgColor }}>
-          <span className='h1 center-section-text'>{text}</span>
-        </div>
-      </div>
-      <div className='section-image-wrapper'>
+      <div className='section-wrapper' style={{ color: color.fgColor }}>
+        <span className='section-text'>{text}</span>
         <img className='section-image' src={img} alt='about text'></img>
       </div>
     </div>
